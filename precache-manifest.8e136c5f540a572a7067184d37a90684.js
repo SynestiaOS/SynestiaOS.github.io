34291b81fe31a4291038b95892692bab2a1533db
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-29 10:00:01 +0800

    [XingfengYang] #N/A

---
+++ precache-manifest.8e136c5f540a572a7067184d37a90684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb63e0ea53cc2f69957e0f72557456e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568a472c5edc7d170d91",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "95c59e7c7580b25a694e",
+    "url": "/static/css/main.7e1898ac.chunk.css"
+  },
+  {
+    "revision": "568a472c5edc7d170d91",
+    "url": "/static/js/2.e7ab96d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e7ab96d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c59e7c7580b25a694e",
+    "url": "/static/js/main.9e21f877.chunk.js"
+  },
+  {
+    "revision": "c20a2c14bc8a0e9b27ce",
+    "url": "/static/js/runtime-main.e8297b11.js"
+  }
+]);
