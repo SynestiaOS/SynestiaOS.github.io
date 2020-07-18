adc6377246cde85d9afcc8dc75dd378fb451bb10
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-07-18 17:31:45 +0800

    [XingfengYang] #N/A

---
+++ precache-manifest.7aa17e2cd6d11048277bb959cdb1b1ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbee8ffd820d957500fe02e6fa363f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d6f806c45021672d34",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "5109640922ac1388b7f5",
+    "url": "/static/css/main.30d9b738.chunk.css"
+  },
+  {
+    "revision": "12d6f806c45021672d34",
+    "url": "/static/js/2.ed06f13b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ed06f13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5109640922ac1388b7f5",
+    "url": "/static/js/main.fd955fe1.chunk.js"
+  },
+  {
+    "revision": "c20a2c14bc8a0e9b27ce",
+    "url": "/static/js/runtime-main.e8297b11.js"
+  },
+  {
+    "revision": "53cbc7a57ccb73cffa8cffbd394781a3",
+    "url": "/static/media/ZillaSlab-Regular.53cbc7a5.otf"
+  },
+  {
+    "revision": "ad3ba53a3bd6f3df2ed3c31ea51c331e",
+    "url": "/static/media/iot.ad3ba53a.png"
+  },
+  {
+    "revision": "f29ecd8e5b93f51210dea7fec176b9b3",
+    "url": "/static/media/lg.f29ecd8e.jpg"
+  },
+  {
+    "revision": "23459da2053a9c27bb1d2643cab44040",
+    "url": "/static/media/ll.23459da2.png"
+  }
+]);
