97b587bcb2c2e04ed8062c37c300c67aa4ab2a7b
NeoYang <nerosoft@outlook.com>
2020-12-01 00:56:52 +0800

    [XingfengYang] #N/A

--- precache-manifest.75725247e32ece2caf4113c24d2c7d50.js
+++ precache-manifest.75725247e32ece2caf4113c24d2c7d50.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f0cc55c09d80bcfb5012b38f6588641",
+    "revision": "9204324d43a050259fc31b1c398c8d94",
     "url": "/index.html"
   },
   {
-    "revision": "09d8e7420308084e953c",
+    "revision": "8910ab67de52e6cf8b85",
     "url": "/static/css/2.300c0e58.chunk.css"
   },
   {
-    "revision": "d5d5887a055153ac4a0e",
+    "revision": "247eade23beff98102aa",
     "url": "/static/css/main.77775b1e.chunk.css"
   },
   {
-    "revision": "09d8e7420308084e953c",
-    "url": "/static/js/2.29586eda.chunk.js"
+    "revision": "8910ab67de52e6cf8b85",
+    "url": "/static/js/2.17d3673a.chunk.js"
   },
   {
     "revision": "e96d67f6ede3d6abbada55eaebf778b8",
-    "url": "/static/js/2.29586eda.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.17d3673a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d5d5887a055153ac4a0e",
-    "url": "/static/js/main.678d601f.chunk.js"
+    "revision": "247eade23beff98102aa",
+    "url": "/static/js/main.2de949f5.chunk.js"
   },
   {
     "revision": "6981919806c089cef906",
